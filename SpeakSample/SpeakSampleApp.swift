90d2cf9a2af5ae438e848198faefe90e07628a3c
Haruki Hanagata <haruzo28@icloud.com>
2023-01-20 06:02:26 +0900

    Initial Commit

---
+++ SpeakSample/SpeakSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakSampleApp.swift
+//  SpeakSample
+//
+//  Created by 花形春輝 on 2023/01/20.
+//
+
+import SwiftUI
+
+@main
+struct SpeakSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
